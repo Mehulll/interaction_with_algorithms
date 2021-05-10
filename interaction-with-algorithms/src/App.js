0afcdaf7ccf39dfbd0a7f36b9b44480db627011c
Mehulll <mehul.aryan.2222@gmail.com>
2021-05-10 17:28:44 +0530

    setting up a react app

---
+++ interaction-with-algorithms/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'react-router-dom/Link';
+import React from 'react';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+function App() {
+  return (
+   <PathfindingVisualizer></PathfindingVisualizer>
+  );
+}
+
+export default App;
